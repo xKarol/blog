d143a9c0a1759ee0cfb1b401866acc3976550b77
xKarol <karolpiskorz8@gmail.com>
2022-05-06 14:57:47 +0200

    Add header logged in render content

--- src/classes/header.js
+++ src/classes/header.js
@@ -4,6 +4,26 @@ export class Header {
 
   updateButtons(logged = false) {
     if (logged) {
+      const avatar = document.createElement("img");
+      avatar.src =
+        "https://kis.agh.edu.pl/wp-content/uploads/2021/01/default-avatar.jpg";
+      avatar.alt = "avatar";
+      avatar.classList = "header__auth__avatar";
+      Header.headerAuthElement.appendChild(avatar);
+
+      const container = document.createElement("div");
+      container.classList = "header__auth__content";
+      Header.headerAuthElement.appendChild(container);
+
+      const username = document.createElement("span");
+      username.classList = "header__auth__content__username";
+      username.innerText = "John Doe";
+      container.appendChild(username);
+
+      const email = document.createElement("span");
+      email.classList = "header__auth__content__email";
+      email.innerText = "johndoe@gmail.com";
+      container.appendChild(email);
     } else {
       this.#createButton("Sign In", "/sign-in.html", "header__auth__link");
 
