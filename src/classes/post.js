43e3c362ad55d1803fffff5a34ad1f6ae87b00eb
xKarol <karolpiskorz8@gmail.com>
2022-05-08 14:55:34 +0200

    Add post page title element

--- src/classes/post.js
+++ src/classes/post.js
@@ -4,11 +4,19 @@ import { getDoc, doc } from "firebase/firestore";
 export class Post {
   constructor(id) {
     this.id = id;
-    this.#fetch();
+    this.#fetch().then(() => this.#render());
   }
+
   async #fetch() {
     const db = App.db;
     const docData = await getDoc(doc(db, "posts", this.id));
-    console.log(docData.data());
+    this.data = docData.data();
+    console.log(this.data);
   }
+
+  #render() {
+    const { title } = this.data;
+    const titleElement = document.querySelector("#post-title");
+    titleElement.innerText = title;
+    titleElement.setAttribute("data-text", title);
 }
