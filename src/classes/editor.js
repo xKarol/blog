367891913f2d4eacc68b3040c1c616c429ca082e
xKarol <karolpiskorz8@gmail.com>
2022-05-22 14:11:43 +0200

    Add editor class

---
+++ src/classes/editor.js
@@ -0,0 +1,42 @@
+export class Editor {
+  constructor(root) {
+    this.root = root;
+    this.tags = ["JavaScript", "HTML", "CSS"];
+    this.#render();
+  }
+
+  #renderTags() {
+    const tagsEl = document.createElement("form");
+    tagsEl.className = "editor__tag";
+    this.tags.forEach((tag) => {
+      const tagEl = document.createElement("div");
+      tagEl.className = "editor__tag__chip";
+      const tagTextEl = document.createElement("span");
+      tagTextEl.innerText = tag;
+      tagEl.appendChild(tagTextEl);
+      const tagRemoveEl = document.createElement("i");
+      tagRemoveEl.className = "uil uil-times editor__tag__chip__icon";
+      tagEl.appendChild(tagRemoveEl);
+      tagsEl.appendChild(tagEl);
+    });
+    const tagAddEl = document.createElement("input");
+    tagAddEl.type = "text";
+    tagAddEl.className = "editor__tag__input";
+    tagAddEl.placeholder = "Add tag...";
+    tagAddEl.id = "editor-tag";
+    tagsEl.appendChild(tagAddEl);
+    return tagsEl;
+  }
+
+  #render() {
+    const titleEl = document.createElement("input");
+    titleEl.type = "text";
+    titleEl.className = "editor__title";
+    titleEl.placeholder = "New post title here...";
+    titleEl.id = "editor-title";
+    this.root.appendChild(titleEl);
+
+    const tagsEl = this.#renderTags();
+    this.root.appendChild(tagsEl);
+  }
+}
--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,4 +1,5 @@
 import { App } from "../classes/app";
+import { Editor } from "../classes/editor";
 import { Header } from "../classes/header";
 import { User } from "../classes/user";
 
@@ -7,11 +8,13 @@ App.init(() => {
   User.init(() => {
     header.render();
   });
+
+  // const root = document.querySelector("#editor-container");
+  // new Editor(root);
+
   const iframe = document.querySelector("#editor-iframe");
   iframe.contentDocument.body.style.color = "#fff";
   iframe.contentDocument.body.style.fontFamily = "sans-serif";
   iframe.contentDocument.designMode = "on";
-  // setInterval(() => {
-  console.log(iframe.contentDocument.body.innerHTML);
-  // }, 200);
+  // console.log(iframe.contentDocument.body.innerHTML);
 });
