d94987f04b4d596850d9d3272faada88ac0f5cf0
xKarol <karolpiskorz8@gmail.com>
2022-05-24 13:24:20 +0200

    Add text editor formatting

--- src/classes/text-editor.js
+++ src/classes/text-editor.js
@@ -28,58 +28,72 @@ export class TextEditor {
     editorHeader.className = "editor__header";
     editorHeader.innerHTML = `
           <div class="editor__header__btns-group">
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="bold">
               <i class="uil uil-bold"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="italic">
               <i class="uil uil-italic"></i>
             </button>
-            <button class="editor__header__button">
-              <i class="uil uil-text-fields"></i>
+            <button type="button" class="editor__header__button" data-type="underline">
+              <i class="uil uil-underline"></i>
             </button>
           </div>
           <div class="editor__header__divider"></div>
           <div class="editor__header__btns-group">
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="justifyLeft">
               <i class="uil uil-align-left"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="justifyCenter">
               <i class="uil uil-align-center"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="justifyRight">
               <i class="uil uil-align-right"></i>
             </button>
           </div>
           <div class="editor__header__divider"></div>
           <div class="editor__header__btns-group">
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="insertUnorderedList">
               <i class="uil uil-list-ul"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="line-spacing">
               <i class="uil uil-line-spacing"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="code">
               <i class="uil uil-arrow"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="emote">
               <i class="uil uil-smile"></i>
             </button>
           </div>
           <div class="editor__header__divider"></div>
           <div class="editor__header__btns-group">
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="image">
               <i class="uil uil-image"></i>
             </button>
-            <button class="editor__header__button">
+            <button type="button" class="editor__header__button" data-type="link">
               <i class="uil uil-link-h"></i>
             </button>
           </div>
           <div class="editor__header__divider"></div>
-          <button class="editor__header__button">
+          <button type="button" class="editor__header__button" data-type="more">
             <i class="uil uil-ellipsis-v"></i>
           </button>
     `;
+    editorHeader.addEventListener("click", (e) =>
+      this.#handleClickHeader(e, this)
+    );
     return editorHeader;
+  }
+
+  #handleClickHeader(e, getThis) {
+    const element = e.target;
+    const type = element.dataset.type;
+
+    if (type === "image") {
+    } else {
+      getThis.iframeEl.contentDocument.execCommand(type, false, null);
+    }
+    this.selectedBtn = type;
   }
 
   getContent() {
